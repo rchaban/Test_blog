590e939ed99047fa1d09d884375c6b16707e5215
rchaban <roman18888@gmail.com>
2015-03-21 20:09:46 +0200

    Finish layout, routes and views

--- Gemfile
+++ Gemfile
@@ -3,8 +3,8 @@ source 'https://rubygems.org'
 gem "haml-rails", "~> 0.9"
 gem 'rails', '4.2.0'
 
-gem 'mysql2'
-
+gem 'sqlite3'
+gem 'bootstrap-sass', '2.3.2.0'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   resources :articles do
     resources :comments
   end  
   get 'welcome/index'
+  match '/about', to: 'welcome#about', via: 'get'
+  match '/contact', to: 'welcome#contact', via: 'get'
+  match '/signup', to: 'users#new', via: 'get'
 
   
 end
