f53b7e7b95944bac36de38f524c8b7f782d14a9c
rchaban <roman18888@gmail.com>
2015-03-23 17:31:50 +0200

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   resources :users
   resources :articles do
     resources :comments
-  end  
+  end 
+  resources :sessions, only: [:new, :create, :destroy] 
   get 'welcome/index'
   match '/about', to: 'welcome#about', via: 'get'
   match '/contact', to: 'welcome#contact', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
-
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150322113159) do
+ActiveRecord::Schema.define(version: 20150323135602) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -36,7 +36,10 @@ ActiveRecord::Schema.define(version: 20150322113159) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.string   "password_digest", limit: 255
+    t.string   "remember_token",  limit: 255
   end
+
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
   add_foreign_key "comments", "articles"
 end
